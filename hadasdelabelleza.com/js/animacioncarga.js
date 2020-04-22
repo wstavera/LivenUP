df7e4c79a4aea6b5da38f2b3b4eaa7d5faca8f5d
wstavera <ws.tavera@gmail.com>
2020-04-22 16:50:43 -0500

    a
    a

---
+++ hadasdelabelleza.com/js/animacioncarga.js
@@ -0,0 +1,10 @@
+wow = new WOW(
+    {
+      animateClass: 'animated',
+      offset:       100,
+      callback:     function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+      }
+    }
+  );
+  wow.init();
