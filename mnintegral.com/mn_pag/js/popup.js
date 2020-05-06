a64dd9d7832da5022e653819cb4b1be88a4e47d7
wstavera <ws.tavera@gmail.com>
2020-05-06 14:25:20 -0500

    a
    a

--- mnintegral.com/mn_pag/js/popup.js
+++ mnintegral.com/mn_pag/js/popup.js
@@ -1,46 +1,197 @@
-// js only creates and insert parts + click events adds classes
+/* diapo 1 */
 
-var numOfPieces = 6 * 6;
-
-var frag = document.createDocumentFragment();
-
-function insertInnerPieces($el, innerPieces) {
-  for (var i = 0; i < innerPieces; i++) {
-    var $inner = document.createElement('div');
-    $inner.classList.add('popup__piece-inner');
-    $el.appendChild($inner);
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("img01");
+  
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    /* captionText.innerHTML = this.alt; */
   }
-};
-
-for (var i = 1; i <= numOfPieces; i++) {
-  var $piece = document.createElement('div');
-  $piece.classList.add('popup__piece');
   
-  insertInnerPieces($piece, 3);
-  frag.appendChild($piece);
-}
-
-document.querySelector('.popup__pieces').appendChild(frag);
-
-var $popupsCont = document.querySelector('.popups-cont');
-var $popup = document.querySelector('.popup');
-var popupAT = 900;
-
-document.querySelector('.popup-btn').addEventListener('click', function() {
-  $popupsCont.classList.add('s--popup-active');
-  $popup.classList.add('s--active');
-});
-
-function closeHandler() {
-  $popupsCont.classList.remove('s--popup-active');
-  $popup.classList.remove('s--active');
-  $popup.classList.add('s--closed');
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
   
-  setTimeout(function() {
-    $popup.classList.remove('s--closed');
-  }, popupAT);
-}
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
 
-document.querySelector('.popup__close').addEventListener('click', closeHandler);
 
-document.querySelector('.popups-cont__overlay').addEventListener('click', closeHandler);
+  /* diapo 2 */
+
+  // Get the modal
+  var modal2 = document.getElementById("myModal2");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img2 = document.getElementById("myImg2");
+  var modalImg2 = document.getElementById("img02");
+  
+  img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span2 = document.getElementsByClassName("close2")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span2.onclick = function() { 
+    modal2.style.display = "none";
+  }
+
+
+  
+  /* diapo 3 */
+
+  // Get the modal
+  var modal3 = document.getElementById("myModal3");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img3 = document.getElementById("myImg3");
+  var modalImg3 = document.getElementById("img03");
+  
+  img3.onclick = function(){
+    modal3.style.display = "block";
+    modalImg3.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span3 = document.getElementsByClassName("close3")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span3.onclick = function() { 
+    modal3.style.display = "none";
+  }
+
+  
+  /* diapo 4 */
+
+  // Get the modal
+  var modal4 = document.getElementById("myModal4");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img4 = document.getElementById("myImg4");
+  var modalImg4 = document.getElementById("img04");
+  
+  img4.onclick = function(){
+    modal4.style.display = "block";
+    modalImg4.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span4 = document.getElementsByClassName("close4")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span4.onclick = function() { 
+    modal4.style.display = "none";
+  }
+
+
+  
+  /* diapo 5 */
+
+  // Get the modal
+  var modal5 = document.getElementById("myModal5");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img5 = document.getElementById("myImg5");
+  var modalImg5 = document.getElementById("img05");
+  
+  img5.onclick = function(){
+    modal5.style.display = "block";
+    modalImg5.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span5 = document.getElementsByClassName("close5")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span5.onclick = function() { 
+    modal5.style.display = "none";
+  }
+
+
+  
+  /* diapo 6 */
+
+  // Get the modal
+  var modal6 = document.getElementById("myModal6");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img6 = document.getElementById("myImg6");
+  var modalImg6 = document.getElementById("img06");
+  
+  img6.onclick = function(){
+    modal6.style.display = "block";
+    modalImg6.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span6 = document.getElementsByClassName("close6")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span6.onclick = function() { 
+    modal6.style.display = "none";
+  }
+
+
+
+  
+  /* diapo 7 */
+
+  // Get the modal
+  var modal7 = document.getElementById("myModal7");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img7 = document.getElementById("myImg7");
+  var modalImg7 = document.getElementById("img07");
+  
+  img7.onclick = function(){
+    modal7.style.display = "block";
+    modalImg7.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span7 = document.getElementsByClassName("close7")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span7.onclick = function() { 
+    modal7.style.display = "none";
+  }
+
+
+  
+  /* diapo 8 */
+
+  // Get the modal
+  var modal8 = document.getElementById("myModal8");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img8 = document.getElementById("myImg8");
+  var modalImg8 = document.getElementById("img08");
+  
+  img8.onclick = function(){
+    modal8.style.display = "block";
+    modalImg8.src = this.src;
+    /* captionText.innerHTML = this.alt; */
+  }
+  
+  // Get the <span> element that closes the modal
+  var span8 = document.getElementsByClassName("close8")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span8.onclick = function() { 
+    modal8.style.display = "none";
+  }
+
